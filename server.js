6578bce973705afcafee6b6cbf2a2d378f779c91
Sikander Ahmed <98826085+skndrahmd@users.noreply.github.com>
2023-02-11 09:46:36 +0500

    final commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.json())
-const prompt = require('prompt-sync')
+
 
 
 const envelopeRouter = require('./envelopes');
